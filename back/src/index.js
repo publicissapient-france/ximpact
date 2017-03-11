860fee31542cd632ae6ba73a29e539931faa17b6
Benjamin Lacroix <benjlacroix@gmail.com>
2017-03-11 11:59:37 +0100

    Allow CORS on localhost:8080 and POST

--- back/src/index.js
+++ back/src/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const query = require('./queries');
 const mutation = require('./mutations');
-const { GraphQLSchema } = require('graphql');
+const {GraphQLSchema} = require('graphql');
 
-const schema = new GraphQLSchema({ query, mutation });
+const schema = new GraphQLSchema({query, mutation});
 
-const root = { hello: () => 'Hello world!' };
+const root = {hello: () => 'Hello world!'};
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  next();
+});
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: root,
