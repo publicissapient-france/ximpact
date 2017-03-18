868154046efc29852d8d72eccf1487961115ef3a
jsmadja <julien.smadja@gmail.com>
2017-03-18 15:07:12 +0100

    Update schema script

---
+++ back/scripts/update-schema.js
@@ -0,0 +1,104 @@
+const rp = require('request-promise');
+const options = {
+  method: 'POST',
+  uri: 'http://localhost:4000/graphql',
+  body: {
+    query: `
+query IntrospectionQuery {
+    __schema {
+      queryType { name }
+      mutationType { name }
+      subscriptionType { name }
+      types {
+        ...FullType
+      }
+      directives {
+        name
+        description
+        locations
+        args {
+          ...InputValue
+        }
+      }
+    }
+  }
+  fragment FullType on __Type {
+    kind
+    name
+    description
+    fields(includeDeprecated: true) {
+      name
+      description
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+      isDeprecated
+      deprecationReason
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+      description
+      isDeprecated
+      deprecationReason
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+  fragment InputValue on __InputValue {
+    name
+    description
+    type { ...TypeRef }
+    defaultValue
+  }
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+  },
+  json: true // Automatically stringifies the body to JSON
+};
+
+rp(options)
+  .then(parsedBody => console.log(JSON.stringify(parsedBody)))
+  .catch(function (err) {
+    console.error(err);
+  });
