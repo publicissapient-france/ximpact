285ab6f19ec839461b156e1a01c2927dfd764285
jsmadja <julien.smadja@gmail.com>
2017-03-19 22:40:29 +0100

    Back : Create feedbacks

--- back/src/monthly-feedback/monthly-feedback.js
+++ back/src/monthly-feedback/monthly-feedback.js
@@ -1,5 +1,11 @@
+const ImpactRepository = require('../impact/repository.impact');
+const FeedbackRepository = require('../feedback/repository.feedback');
+
 module.exports = {
   sendFeedbackRequest: () => {
     console.log('It\'s time to feedback');
+    ImpactRepository
+      .getImpactsToFeedback()
+      .then(impacts => impacts.forEach(impact => FeedbackRepository.addFeedback(impact.xebianId, impact.id)));
   },
 };
